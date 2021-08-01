58643cc648ff0cfca32dfc3f6ec70d5dc8148b62
Cippo95 <cippo1995@gmail.com>
2021-08-01 18:07:43 +0200

    getvectors ok riguardo codice e docs

--- getvectors.py
+++ getvectors.py
@@ -1,31 +1,74 @@
+#la libreria sys permette di usare gli argomenti a riga di comando
 import sys
-# import re
 
+#controllo che venga passato al programma il file prodotto da HOPE da analizzare
 if len(sys.argv) != 2:
     raise ValueError("Specifica il file da leggere come argomento!")
 
-#gli argomenti sono stringhe, converto in int
+#assegno a file_name il nome del file prodotto da HOPE
 file_name = sys.argv[1]
 
-# Using for loop
+#nome del file in cui salvo gli ingressi le uscite precedenti e i guasti rivelati per ogni test
+fn_array = (f"vectors_{file_name}")
+
+#al fine di fare dei calcoli statistici introduco dei contatori
+
+#contatore per i guasti rivelati
 count_fault = 0
-count_line = 0
-print("\nUsing for loop")
- 
+#contatore dei test totali (sono guasti iniettati)
+count_test = 0
+#uso questo array per salvare i vari test
+array = []
+
 #apertura del file in lettura 
 with open(file_name) as fp:
     for line in fp:
         if 'test' in line:
-            if(count_fault!=0 and count_line!=0):
-                test_fault_coverage=(count_fault/count_line)*100
-                print("counted faults: {}".format(count_fault),
-                "\ncounted test vectors: {}".format(count_line),
-                "\nfault_detected: {}%".format(test_fault_coverage))
-            count_fault = 0
-            count_line = 0
+            #se sono arrivato al test successivo calcolo le statistiche del test precedente
+            #controllo che siano stati iniettati dei guasti per poter fare i calcoli
+            if(count_test!=0):
+                #copertura di guasto con questo test
+                test_fault_coverage=(count_fault/count_test)*100
+                print("guasti rivelati: {}".format(count_fault),
+                "\nguasti iniettati: {}".format(count_test),
+                "\npercentuale di rivelazione: {}%".format(test_fault_coverage))
+                #azzero i contatori
+                count_fault = 0
+                count_test = 0
+                array.append(array_test)
+            #preparo un array per i risultati di questo test
+            array_test = []
+            #scrivo il numero di test e gli ingressi e l'uscita precedente
             print("\n{}".format(line.strip()))
+            #split separa la stringa in due
+            input_output = line.split(':')
+            #concateno in una unica riga:
+            #1) prendo la seconda parte con gli ingressi e l'uscita precedente
+            #2) strip toglie gli spazi intorno
+            #3) split separa i vettori considerando lo spazio centrale
+            input_output = input_output[1].strip().split()
+            #inserisco gli ingressi e l'uscita precedente nel array dei risultati del test corrente
+            array_test.append(input_output[0])
+            array_test.append(input_output[1])
         else:
-            count_line += 1
+            #sono in una linea di test per cui conto
+            count_test += 1
+            #se un guasto è rivelato ha l'asterisco
             if '*' in line:
+                #conto il guasto rivelato
                 count_fault += 1
+                #scrivo a terminale
                 print("{}".format(line.strip()))
+                #recupero il vettore di rivelazione separando la stringa sull'asterisco
+                test=line.split('*')
+                #prendo la seconda parte (quella col vettore) e tolgo gli spazi
+                test=test[1].strip()
+                #aggiungo il vettore all'array dei risultati correnti
+                array_test.append(test)
+#salvo l'array dei vettori di test
+with open(fn_array, "w") as fp:
+    for line in array:
+        fp.write(str(line))
+#NB:ogni lista ha come primi due elementi gli ingressi e l'uscita precedente e poi le rivelazioni di guasto.
+
+
--- mac_generator.py
+++ mac_generator.py
@@ -1,33 +1,37 @@
+#sys serve per usare gli argomenti
 import sys
 
+#controllo che sia stato dato un argomento che è il numero di bit
 if len(sys.argv) != 2:
-    raise ValueError("Specifica i bit come argomento!")
+    raise ValueError("Specifica i bit degli ingressi del circuto come argomento!")
 
-#gli argomenti sono stringhe, converto in int
+#l'argomento è visto come una stringa quindi lo converto in intero
 bit = int(sys.argv[1])
 
-#assegno un nome al file parametrico coi bit, estensione .bench per essere usato con hope
+#assegno un nome al file, parametrico coi bit, estensione .bench per essere usato con hope
 file_name = (f'mac_{bit}.bench')
 
 #apertura del file in scrittura
 f = open(file_name, "w")
 
-#scrivo il nome del circuto, serve ad hope
+#scrivo il nome del circuto, cosa che serve ad hope
 f.write('# multiply_and_accumulate_unit\n\n# Input\n')
 
-#generazione degli input
+#genero gli input separatamente per avere più ordine nei file generati da hope che dovrò poi manipolare per gli studi statistici, generando gli input con un solo ciclo si mischierebbero
+#generazione del input X, chiaro ciclo for parametrico con il numero di bit
 for i in range(bit):
     text_generated = ('\n'
     f'INPUT(X{bit-i-1})')
     f.write(text_generated)
-#generazione degli input
+#generazione del input Y, chiaro ciclo for parametrico con il numero di bit
 for i in range(bit):
     text_generated = ('\n'
     f'INPUT(Y{bit-i-1})')
     f.write(text_generated)
 
-#commento e costruisco la prima parte del moltiplicatore (i vari and) come da documentazione
+#commento e costruisco la prima parte del moltiplicatore (i vari AND che originano i segnali W) come da documentazione
 f.write('\n\n# 1) Prima parte del moltiplicatore: AND che generano i segnali W\n')
+#Ogni X va in AND con ogni Y, credo sia ovvio il doppio ciclo for
 for i in range(bit):
     for j in range(bit):
         text_generated = ('\n'
@@ -37,12 +41,15 @@ for i in range(bit):
 #commento e costruisco la seconda parte del moltiplicatore (rca in cascata) come da documentazione
 f.write('\n\n# 2) Seconda parte del moltiplicatore: cascata di rca shiftati\n')
 
-#distinguo rca in livelli, intuitivo guardando la documentazione, livelli sono 'bit - 1'
+#distinguo rca per livelli
+#i livelli sono 'bit - 1' per come è strutturato il circuito
+#la variabile "i" indica il livello
 for i in range(bit-1):
+    #commento per segnalare i livelli
     f.write(f'\n# RCA LIVELLO {i}:\n')
     #distinguo i bit su cui vado a lavorare
     for j in range(bit):
-        #scrivo queste variabili per aiutarmi, so che può non piacere avere variabili letterali ma credo sia intuitivo il significato
+        #scrivo queste variabili per aiutarmi a gestire la combinazione dei vari segnali che possono essere di un livello o posizione prima o dopo.
         a=i+1
         b=j+1
         c=i-1
@@ -53,38 +60,49 @@ for i in range(bit-1):
             if j==0:
                 f.write(f'\n# HALFADDER LIV {i} BIT {j}')
                 text_generated=('\n'
+                #S è l'uscita come da documentazione, per esempio S0
+                #L sta per "Level" cioè il livello 
+                #D sta per "Depth" cioè profondità, indica la posizione del bit
+                #esempio: al livello 0 abbiamo S0, nel file sarà SL0D0
+                #esempio: S0 è lo XOR di W01 e W10 -> SL0D0 = W_X0Y1 e W_X1Y0
                 f'SL{i}D{j} = XOR(W_X{j}Y{a}, W_X{b}Y{i})\n'
+                #"Co" è il Carry-Out, L e D hanno lo stesso significato di prima
                 f'CoL{i}D{j} = AND(W_X{j}Y{a}, W_X{b}Y{i})\n')
             #gli altri sono fulladder tranne l'ultimo che ha un ingresso 0 quindi è un halfadder
             else:
-                #variabili per collegare i segnali W
+                #variabili per risparmiarmi un po' di scrittura, coincidono con gli A e B degli schemi
                 A=f'W_X{j}Y{a}'
                 B=f'W_X{b}Y{i}'
+                #se non è l'ultimo ho dei fulladder
                 if j!=bit-1:
                     f.write(f'\n# FULLADDER LIV {i} BIT {j}')
                     text_generated=('\n'
+                    #1,2,3 indicano i segnali interni del fulladder, come riportato nei miei schemi
                     f'1L{i}D{j} = XOR({A}, {B})\n'
                     f'2L{i}D{j} = AND({A}, {B})\n'
                     f'3L{i}D{j} = AND(1L{i}D{j}, CoL{i}D{d})\n'
                     f'SL{i}D{j} = XOR(CoL{i}D{d}, 1L{i}D{j})\n'
                     f'CoL{i}D{j} = OR(3L{i}D{j}, 2L{i}D{j})\n')
-                #l'ultimo in questo caso è un halfadder
+                #l'ultimo in questo caso è un halfadder perché l'ultimo fulladder al livello 0 ha un ingresso a 0
                 else:
                     f.write(f'\n# Al livello 0 l\'ultimo fulladder avrebbe ingresso 0 quindi metto un halfadder\n# HALFADDER LIV {i} BIT {j}')
                     text_generated=('\n'
                     f'CoL{i}D{j} = AND({A}, CoL{i}D{d})\n'
                     f'SL{i}D{j} = XOR(CoL{i}D{d}, {A})\n')
-        #se non sono al primo livello cambio alcuni segnali e condizioni, intuitivo da documentazione
+        #se non sono al primo livello dovrò prendere alcuni segnali dal livello precedente e cambiare qualche condizione
+        #Ad esempio un ingresso è il segnale W succesivo mentre l'altro è l'uscita del rca al livello precedente
         else:
             if j==0:
                 f.write(f'\n# HALFADDER LIV {i} BIT {j}')
                 text_generated=('\n'
+                #come si vede qui prendo l'uscita precedente S
                 f'SL{i}D{j} = XOR(W_X{j}Y{a}, SL{c}D{b})\n'
                 f'CoL{i}D{j} = AND(W_X{j}Y{a}, SL{c}D{b})\n')
             else:
                 if j!=bit-1:
                     f.write(f'\n# FULLADDER LIV {i} BIT {j}')
                     A=f'W_X{j}Y{a}'
+                    #anche qui uscita precedente S
                     B=f'SL{c}D{b}'
                     text_generated=('\n'
                     f'1L{i}D{j} = XOR({A}, {B})\n'
@@ -93,6 +111,7 @@ for i in range(bit-1):
                     f'SL{i}D{j} = XOR(CoL{i}D{d}, 1L{i}D{j})\n'
                     f'CoL{i}D{j} = OR(3L{i}D{j}, 2L{i}D{j})\n')
                 else:
+                    #se ho l'ultimo full-adder in un livello diverso dal primo prendo il CO dal livello precedente
                     f.write(f'\n# FULLADDER LIV {i} BIT {j}')
                     A=f'W_X{j}Y{a}'
                     B=f'CoL{c}D{j}'
@@ -116,6 +135,7 @@ for j in range(2*bit):
         #al primo bit ho un halfadder che va a sommare W00 e il primo bit del registro
         f.write(f'\n# HALFADDER RCA BIT {j}')
         text_generated=('\n'
+        #R è l'uscita del registro
         f'S{j} = XOR(W_X{0}Y{0}, R{j})\n'
         f'Co{j} = AND(W_X{0}Y{0}, R{j})\n')
         f.write(text_generated)
@@ -152,10 +172,12 @@ for j in range(2*bit):
                 f'S{j} = XOR(Co{d}, 1{j})\n')
                 # f'Co{j} = OR(3{j}, 2{j})\n')
                 f.write(text_generated)
-
+#costruisco il registro
 f.write(f'\n# 4) Registro R di {2*bit} bit')
+#ovviamente di grandezza 2n
 for j in range(2*bit):
     text_generated=('\n'
+    #semplicemente prendo le uscite e le metto in un flip flop d
     f'R{j} = DFF(S{j})\n'
     f'OUTPUT(R{2*bit-j-1})')
     f.write(text_generated)
